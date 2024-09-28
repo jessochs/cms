94c406416c2a74779a0f01fda38bd60d15bf0434
jessochs <fac17002@byui.edu>
2024-09-28 15:03:30 -0700

    cms project

---
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent {
+  contacts: Contact[] = [
+    new Contact(1, 'R. Kent Jackson', 'jacksok@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'),
+    new Contact(2, 'Rex Barzee',  'Barzeer@byui.edu', '208-4963768', '../../assets/images/barzeer.jpg')
+  ];
+
+  constructor() {
+    
+  }
+  
+
+}
